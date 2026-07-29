474bd77fdc5e1472b6f8a437e4304a686f68cddc
mryac-g <x.pony@icloud.com>
2026-07-29 16:02:31 +0900

    feat: ペット新規登録フォームを追加
    PetsController#new/#create、登録フォーム、TOPページからのリンクを追加。

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -3,5 +3,7 @@ class Pet < ApplicationRecord
 
   enum species: { dog: 0, cat: 1, rabbit: 2, bird: 3, other: 4 }
 
+  SPECIES_LABELS = { "dog" => "犬", "cat" => "猫", "rabbit" => "うさぎ", "bird" => "鳥", "other" => "その他" }.freeze
+
   validates :name, presence: true
 end
