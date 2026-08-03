bccc3105186be935ad9d4496f758ae8a7f5739c5
mryac-g <x.pony@icloud.com>
2026-08-03 13:24:05 +0900

    feat: ペットページ切り替え機能を追加
    PetsController#showを追加。複数ペットがいる場合はタブで切り替えられる
    ペットごとのページを表示する。ペット登録後はそのペットのページにリダイレクトする。

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,5 +1,10 @@
 class PetsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_pet, only: %i[show]
+
+  def show
+    @pets = current_user.pets
+  end
 
   def new
     @pet = current_user.pets.new
@@ -9,13 +14,17 @@ class PetsController < ApplicationController
     @pet = current_user.pets.new(pet_params)
 
     if @pet.save
-      redirect_to root_path, notice: "#{@pet.name}を登録しました"
+      redirect_to pet_path(@pet), notice: "#{@pet.name}を登録しました"
     else
       render :new, status: :unprocessable_entity
     end
   end
 
   private
+
+  def set_pet
+    @pet = current_user.pets.find(params[:id])
+  end
 
   def pet_params
     params.require(:pet).permit(:name, :species, :species_note, :birthday, :icon_url)
