458d9793960d8aa0c151404d3d7af6c1c45ba4d3
mryac-g <x.pony@icloud.com>
2026-08-09 18:11:47 +0900

    プリセット追加後、記録フォームのボタン一覧に即座に反映されるようにする
    モーダル内でプリセットの追加・削除が行われた場合、モーダルを
    閉じたタイミングでTurboによるソフトなページ再取得を行い、記録
    フォームのボタン一覧を最新化するようにした。変更が無い場合は
    再取得しない。
    
    closes #119

--- app/javascript/controllers/preset_modal_controller.js
+++ app/javascript/controllers/preset_modal_controller.js
@@ -13,4 +13,12 @@ export default class extends Controller {
     event?.preventDefault()
     this.dialogTarget.close()
   }
+
+  // モーダル内でプリセットの追加・削除が行われていた場合のみ、記録フォームの
+  // ボタン一覧を最新化するためページを再取得する(成功メッセージの有無で判定)
+  refreshIfChanged() {
+    if (this.frameTarget.querySelector(".alert-success")) {
+      Turbo.visit(window.location.href, { action: "replace" })
+    }
+  }
 }
