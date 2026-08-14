8e235ad4ef14d9b107faf4c40ff051b33aea75c8
mryac-g <x.pony@icloud.com>
2026-08-14 14:44:46 +0900

    排泄記録の便の状態を5種類に変更する
    普通・軟便・水様便・出なかった、の4種類から、かたい・ふつう・やわらかい・
    下痢・出なかった、の5種類に変更する。既存データとの整合性のため、既存の
    enum値の番号は変更せず、新しいhardのみ末尾に追加している。
    
    Closes #172

--- test/models/toilet_test.rb
+++ test/models/toilet_test.rb
@@ -16,4 +16,14 @@ class ToiletTest < ActiveSupport::TestCase
     assert toilet.valid?
     assert_nil toilet.condition
   end
+
+  test "valid with the hard condition" do
+    toilet = Toilet.new(care_record: care_records(:one), kind: :poop, condition: :hard)
+    assert toilet.valid?
+  end
+
+  test "CONDITION_OPTIONS lists the five expected conditions in order" do
+    assert_equal %w[hard normal soft watery absent], Toilet::CONDITION_OPTIONS.map(&:first)
+    assert_equal ["かたい", "ふつう", "やわらかい", "下痢", "出なかった"], Toilet::CONDITION_OPTIONS.map { |o| o[1] }
+  end
 end
