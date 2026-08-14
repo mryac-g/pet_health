bec91051d6e04f53b410cc9669dc7425f8a2e7ff
mryac-g <x.pony@icloud.com>
2026-08-14 20:46:23 +0900

    サイドバーのペット切り替えをどのページでも常に表示する
    ペット詳細ページ(pets#show)以外の下位ページでは表示されていたが、
    ホームなどペットに紐づかないページでは表示されず、サイドバーの
    見た目がページによって変わってしまっていた。ページに関わらず常に
    同じサイドバーになるよう、@pet依存の表示条件を外した。
    
    あわせて、ビュー側での初回参照をBulletにN+1と誤検知されないよう、
    @petsのロードをコントローラのbefore_action側で完了させておく。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,10 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # サイドバーのペット切り替えは、ペット詳細以外の下位ページ(記録一覧・記録詳細・
-  # プロフィール編集等)でも表示したいため、ログイン中は常に@petsを用意しておく
+  # サイドバーのペット切り替えはどのページでも常に表示したいため、ログイン中は
+  # 常に@petsを用意しておく。ビュー側での初回アクセスをBulletにN+1と誤検知され
+  # ないよう、ここ(コントローラ)でロードを済ませておく
   def set_pets_for_sidebar
-    @pets = current_user.pets if user_signed_in?
+    @pets = current_user.pets.to_a if user_signed_in?
   end
 end
