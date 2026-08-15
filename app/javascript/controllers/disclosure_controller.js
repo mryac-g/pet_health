cca0c70c8041dfee54a34199af0adff53564e9ce
mryac-g <x.pony@icloud.com>
2026-08-16 04:54:58 +0900

    ワクチン入力欄を閉じられるようにし、メモ欄をさらに縮める
    - disclosure_controller.jsを双方向トグルにし、展開後に✕ボタンで
      「ワクチンを入力する」ボタンの表示へ戻せるようにする(開いたら戻れない問題の修正)
    - メモ欄はrows:1でもdaisyUIのtextareaに80pxの最小高さがあり見た目が縮まなかったため、
      min-h-0 h-10で40pxまで縮める

--- app/javascript/controllers/disclosure_controller.js
+++ app/javascript/controllers/disclosure_controller.js
@@ -1,12 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 // 「入力する」ボタンをクリックするまで隠しておきたい任意項目(通院のワクチン等)向けの
-// 汎用トグル。ボタンを押すとボタン自身を隠し、中身の入力欄を表示する
+// 汎用トグル。ボタンを押すとボタン自身を隠し、中身の入力欄を表示する。
+// 中の閉じるボタンから同じアクションを呼べば、ボタン表示に戻せる(元に戻れない問題への対応)
 export default class extends Controller {
   static targets = ["trigger", "content"]
 
   toggle() {
-    this.triggerTarget.classList.add("hidden")
-    this.contentTarget.classList.remove("hidden")
+    this.triggerTarget.classList.toggle("hidden")
+    this.contentTarget.classList.toggle("hidden")
   }
 }
